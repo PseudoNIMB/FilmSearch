3c79edf10a2a3448cf92c2e17c7949ba17022e5a
PseudoNIMB <ienndevld@yandex.ru>
2022-09-11 15:05:50 +0300

    Победа против synthetic, активация плагинов в gradle, приложение стабильно.

--- app/src/main/java/ru/pseudonimb/filmsearch/MainActivity.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/MainActivity.kt
@@ -3,7 +3,7 @@ package ru.pseudonimb.filmsearch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import kotlinx.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

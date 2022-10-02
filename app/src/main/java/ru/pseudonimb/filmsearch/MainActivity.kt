3ce3f0dd294c5efecbb22240b64d5c21cdf543bd
PseudoNIMB <ienndevld@yandex.ru>
2022-10-02 17:27:25 +0300

    Toast problem fixed

--- app/src/main/java/ru/pseudonimb/filmsearch/MainActivity.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/MainActivity.kt
@@ -9,9 +9,12 @@ import android.os.Bundle
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initNavigation()
 
         supportFragmentManager
             .beginTransaction()

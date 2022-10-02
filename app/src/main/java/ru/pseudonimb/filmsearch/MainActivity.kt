5c0e8c9ce58a535cbe6f6505175f25b1c6a05512
PseudoNIMB <ienndevld@yandex.ru>
2022-10-02 17:21:43 +0300

    Fragment changes, bugs fixed

--- app/src/main/java/ru/pseudonimb/filmsearch/MainActivity.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/MainActivity.kt
@@ -48,15 +48,15 @@ class MainActivity : AppCompatActivity() {
 
             when (it.itemId) {
                 R.id.favorites -> {
-                    Toast.makeText(this), "Избранное", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
                     true
                 }
                 R.id.watch_later -> {
-                    Toast.makeText(this), "Посмотреть похже", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
                     true
                 }
                 R.id.selections -> {
-                    Toast.makeText(this), "Подборки", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
                     true
                 }
                 else -> false

025b5dd3970e9b450f62778bd427487797ca14df
PseudoNIMB <ienndevld@yandex.ru>
2022-10-22 16:10:23 +0300

    Добавление Glide в код ViewHolder

--- app/src/main/java/ru/pseudonimb/filmsearch/FilmViewHolder.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/FilmViewHolder.kt
@@ -2,6 +2,7 @@ package ru.pseudonimb.filmsearch
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.film_item.view.*
 
 //В конструктор класс передается layout, который мы создали(film_item.xml)
@@ -19,5 +20,13 @@ class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         poster.setImageResource(film.poster)
         //Устанавливаем описание
         description.text = film.description
+        //Указываем контейнер, в котором будет "жить" наша картинка
+        Glide.with(itemView)
+            //Загружаем сам ресурс
+            .load(film.poster)
+            //Центруем изображение
+            .centerCrop()
+            //Указываем ImageView, куда будем загружать изображение
+            .into(poster)
     }
 }

9241e1a0162d96029b656f075206ea99e4fb6263
PseudoNIMB <ienndevld@yandex.ru>
2022-11-20 13:31:07 +0300

    Добавление кода в styles.xml, продолжение изменений по прогрессу

--- app/src/main/java/ru/pseudonimb/filmsearch/RatingDonutView.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/RatingDonutView.kt
@@ -23,6 +23,21 @@ class RatingDonutView @JvmOverloads constructor(context: Context, attributeSet: 
     private lateinit var digitPaint: Paint
     private lateinit var circlePaint: Paint
 
+    init {
+        //Получаем атрибуты и устанавливаем их в соответствующие поля
+        val a =
+            context.theme.obtainStyledAttributes(attributeSet, R.styleable.RatingDonutView, 0, 0)
+        try {
+            stroke = a.getFloat(
+                R.styleable.RatingDonutView_stroke, stroke)
+            progress = a.getInt(R.styleable.RatingDonutView_progress, progress)
+        } finally {
+            a.recycle()
+        }
+        //Инициализируем первоначальные краски
+        initPaint()
+    }
+
     private fun initPaint() {
         //Краска для колец
         strokePaint = Paint().apply {
@@ -51,7 +66,74 @@ class RatingDonutView @JvmOverloads constructor(context: Context, attributeSet: 
         }
     }
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        radius = if (width > height) {
+            height.div(2f)
+        } else {
+            width.div(2f)
+        }
+    }
 
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
+        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
+
+        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
+
+        val chosenWidth = chooseDimension(widthMode, widthSize)
+        val chosenHeight = chooseDimension(heightMode, heightSize)
+
+        val minSide = Math.min(chosenWidth, chosenHeight)
+        centerX = minSide.div(2f)
+        centerY = minSide.div(2f)
+
+        setMeasuredDimension(minSide, minSide)
+    }
+
+    private fun chooseDimension(mode: Int, size: Int) =
+        when (mode) {
+            MeasureSpec.AT_MOST, MeasureSpec.EXACTLY -> size
+            else -> 300
+        }
+
+    override fun onDraw(canvas: Canvas) {
+        //Рисуем кольцо и задний фон
+        drawRating(canvas)
+        //Рисуем цифры
+        drawText(canvas)
+    }
+
+    private fun drawRating(canvas: Canvas) {
+        //Здесь мы можем регулировать размер нашего кольца
+        val scale = radius * 0.8f
+        //Сохраняем канвас
+        canvas.save()
+        //Перемещаем нулевые координаты канваса в центр, вы помните, так проще рисовать все круглое
+        canvas.translate(centerX, centerY)
+        //Устанавливаем размеры под наш овал
+        oval.set(0f - scale, 0f - scale, scale , scale)
+        //Рисуем задний фон(Желательно его отрисовать один раз в bitmap, так как он статичный)
+        canvas.drawCircle(0f, 0f, radius, circlePaint)
+        //Рисуем "арки", из них и будет состоять наше кольцо + у нас тут специальный метод
+        canvas.drawArc(oval, -90f, convertProgressToDegrees(progress), false, strokePaint)
+        //Восстанавливаем канвас
+        canvas.restore()
+    }
+
+
+    private fun drawText(canvas: Canvas) {
+        //Форматируем текст, чтобы мы выводили дробное число с одной цифрой после точки
+        val message = String.format("%.1f", progress / 10f)
+        //Получаем ширину и высоту текста, чтобы компенсировать их при отрисовке, чтобы текст был
+        //точно в центре
+        val widths = FloatArray(message.length)
+        digitPaint.getTextWidths(message, widths)
+        var advance = 0f
+        for (width in widths) advance += width
+        //Рисуем наш текст
+        canvas.drawText(message, centerX - advance / 2, centerY  + advance / 4, digitPaint)
+    }
 
     private fun getPaintColor(progress: Int): Int = when(progress) {
         in 0 .. 25 -> Color.parseColor("#e84258")
@@ -60,4 +142,5 @@ class RatingDonutView @JvmOverloads constructor(context: Context, attributeSet: 
         else -> Color.parseColor("#b0d8a4")
     }
 
+    private fun convertProgressToDegrees(progress: Int): Float = progress * 3.6f
 }

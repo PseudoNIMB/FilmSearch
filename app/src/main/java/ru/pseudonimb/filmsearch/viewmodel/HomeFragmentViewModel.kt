949b4c0e6cdf18f450a77ac5b0edee12530f11b1
PseudoNIMB <ienndevld@yandex.ru>
2022-12-18 19:32:51 +0300

    Инициализация интерактора в HomeFragmentViewModel.kt

--- app/src/main/java/ru/pseudonimb/filmsearch/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/viewmodel/HomeFragmentViewModel.kt
@@ -2,15 +2,15 @@ package ru.pseudonimb.filmsearch.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.pseudonimb.filmsearch.App
 import ru.pseudonimb.filmsearch.domain.Film
 import ru.pseudonimb.filmsearch.domain.Interactor
 
 class HomeFragmentViewModel : ViewModel() {
     val filmsListLiveData = MutableLiveData<List<Film>>()
-    private lateinit var interactor: Interactor
-    init {
-        //TODO init interactor
+    private var interactor: Interactor = App.instance.interactor
 
+    init {
         val films = interactor.getFilmsDB()
         filmsListLiveData.postValue(films)
     }

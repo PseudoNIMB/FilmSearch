32b657f3954fbaf57dacf8c2e98baf5cca52f2f3
PseudoNIMB <ienndevld@yandex.ru>
2022-12-28 22:38:58 +0300

    Попытка всё запустить -_-

--- app/src/main/java/ru/pseudonimb/filmsearch/data/TmdbApi.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/data/TmdbApi.kt
@@ -1,9 +1,10 @@
 package ru.pseudonimb.filmsearch.data
 
+import ru.pseudonimb.filmsearch.data.Entity.TmdbResultsDto
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
-import ru.pseudonimb.filmsearch.data.Entity.TmdbResultsDto
+
 
 interface TmdbApi {
     @GET("3/movie/popular")
--- app/src/main/java/ru/pseudonimb/filmsearch/domain/Interactor.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/domain/Interactor.kt
@@ -1,13 +1,14 @@
 package ru.pseudonimb.filmsearch.domain
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 import ru.pseudonimb.filmsearch.data.API
 import ru.pseudonimb.filmsearch.data.Entity.TmdbResultsDto
 import ru.pseudonimb.filmsearch.data.MainRepository
 import ru.pseudonimb.filmsearch.data.TmdbApi
 import ru.pseudonimb.filmsearch.utils.Converter
 import ru.pseudonimb.filmsearch.viewmodel.HomeFragmentViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class Interactor(private val repo: MainRepository, private val retrofitService: TmdbApi) {
     //В конструктор мы будем передавать коллбэк из вью модели, чтобы реагировать на то, когда фильмы будут получены

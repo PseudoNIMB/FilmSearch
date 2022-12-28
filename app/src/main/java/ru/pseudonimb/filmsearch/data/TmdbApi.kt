735e27eec915d28c459968a21791e559e9b4d413
PseudoNIMB <ienndevld@yandex.ru>
2022-12-28 20:53:19 +0300

    Создание TmdbResultsDto.kt & TmdbFilm.kt, наполнение

--- app/src/main/java/ru/pseudonimb/filmsearch/data/TmdbApi.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/data/TmdbApi.kt
@@ -3,6 +3,7 @@ package ru.pseudonimb.filmsearch.data
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
+import ru.pseudonimb.filmsearch.data.Entity.TmdbResultsDto
 
 interface TmdbApi {
     @GET("3/movie/popular")

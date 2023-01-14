a03ebe6a005395676fba7a309c28e7a5f279b084
PseudoNIMB <ienndevld@yandex.ru>
2023-01-14 14:17:04 +0300

    Наполнение DatabaseModule.kt и RemoteModule.kt

--- app/src/main/java/ru/pseudonimb/filmsearch/di/modules/DatabaseModule.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/di/modules/DatabaseModule.kt
@@ -1,4 +1,13 @@
 package ru.pseudonimb.filmsearch.di.modules
 
+import dagger.Module
+import dagger.Provides
+import ru.pseudonimb.filmsearch.data.MainRepository
+import javax.inject.Singleton
+
+@Module
 class DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideRepository() = MainRepository()
 }

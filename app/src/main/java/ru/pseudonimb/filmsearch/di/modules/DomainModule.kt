5dbda5a806d04812bd8fe81422e356ced37a0166
PseudoNIMB <ienndevld@yandex.ru>
2023-01-22 15:06:43 +0300

    Изменение DomainModule.kt

--- app/src/main/java/ru/pseudonimb/filmsearch/di/modules/DomainModule.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/di/modules/DomainModule.kt
@@ -1,15 +1,27 @@
 package ru.pseudonimb.filmsearch.di.modules
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import ru.pseudonimb.filmsearch.data.MainRepository
+import ru.pseudonimb.filmsearch.data.PreferenceProvider
 import ru.pseudonimb.filmsearch.data.TmdbApi
 import ru.pseudonimb.filmsearch.domain.Interactor
 import javax.inject.Singleton
 
 @Module
-class DomainModule {
+//Передаем контекст для SharedPreferences через конструктор
+class DomainModule(val context: Context) {
+    //Нам нужно контекст как-то провайдить, поэтому создаем такой метод
+    @Provides
+    fun provideContext() = context
+
     @Singleton
     @Provides
-    fun provideInteractor(repository: MainRepository, tmdbApi: TmdbApi) = Interactor(repo = repository, retrofitService = tmdbApi)
+    //Создаем экземпляр SharedPreferences
+    fun providePreferences(context: Context) = PreferenceProvider(context)
+
+    @Singleton
+    @Provides
+    fun provideInteractor(repository: MainRepository, tmdbApi: TmdbApi, preferenceProvider: PreferenceProvider) = Interactor(repo = repository, retrofitService = tmdbApi, preferences = preferenceProvider)
 }

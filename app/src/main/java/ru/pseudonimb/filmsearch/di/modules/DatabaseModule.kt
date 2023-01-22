1dca3126b4ef8f8c8d6ccc58e0d8935db7fa53d2
PseudoNIMB <ienndevld@yandex.ru>
2023-01-22 17:11:29 +0300

    Обновление DI модуля

--- app/src/main/java/ru/pseudonimb/filmsearch/di/modules/DatabaseModule.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/di/modules/DatabaseModule.kt
@@ -1,13 +1,19 @@
 package ru.pseudonimb.filmsearch.di.modules
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
+import ru.pseudonimb.filmsearch.data.DatabaseHelper
 import ru.pseudonimb.filmsearch.data.MainRepository
 import javax.inject.Singleton
 
 @Module
 class DatabaseModule {
+    @Singleton
+    @Provides
+    fun provideDatabaseHelper(context: Context) = DatabaseHelper(context)
+
     @Provides
     @Singleton
-    fun provideRepository() = MainRepository()
+    fun provideRepository(databaseHelper: DatabaseHelper) = MainRepository(databaseHelper)
 }

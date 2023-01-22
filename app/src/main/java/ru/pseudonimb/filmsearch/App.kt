f31d60d2d398ae4fea3228e6684bc8e16815d859
PseudoNIMB <ienndevld@yandex.ru>
2023-01-22 15:13:29 +0300

    Настройки для кнопули

--- app/src/main/java/ru/pseudonimb/filmsearch/App.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/App.kt
@@ -1,8 +1,12 @@
 package ru.pseudonimb.filmsearch
 
 import android.app.Application
+import okhttp3.internal.Internal.instance
 import ru.pseudonimb.filmsearch.di.AppComponent
 import ru.pseudonimb.filmsearch.di.DaggerAppComponent
+import ru.pseudonimb.filmsearch.di.modules.DatabaseModule
+import ru.pseudonimb.filmsearch.di.modules.DomainModule
+import ru.pseudonimb.filmsearch.di.modules.RemoteModule
 
 class App : Application() {
     lateinit var dagger: AppComponent
@@ -11,7 +15,12 @@ class App : Application() {
         super.onCreate()
         instance = this
         //Создаем компонент
-        dagger = DaggerAppComponent.create()
+        dagger = DaggerAppComponent.builder()
+            .remoteModule(RemoteModule())
+            .databaseModule(DatabaseModule())
+            .domainModule(DomainModule(this))
+            .build()
+    }
     }
 
     companion object {

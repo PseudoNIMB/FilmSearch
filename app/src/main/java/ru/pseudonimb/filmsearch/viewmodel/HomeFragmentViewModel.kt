e181c0c8a07c679f0541efd5123481171e1c18b5
PseudoNIMB <ienndevld@yandex.ru>
2023-01-29 14:41:41 +0300

    Изменение DatabaseModule.kt, правка интерактора и HomeFragmentViewModel.kt

--- app/src/main/java/ru/pseudonimb/filmsearch/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/viewmodel/HomeFragmentViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import ru.pseudonimb.filmsearch.App
 import ru.pseudonimb.filmsearch.data.entity.Film
 import ru.pseudonimb.filmsearch.domain.Interactor
+import java.util.concurrent.Executors
 import javax.inject.Inject
 
 class HomeFragmentViewModel : ViewModel() {
@@ -26,7 +27,9 @@ class HomeFragmentViewModel : ViewModel() {
             }
 
             override fun onFailure() {
-                filmsListLiveData.postValue(interactor.getFilmsFromDB())
+                Executors.newSingleThreadExecutor().execute {
+                    filmsListLiveData.postValue(interactor.getFilmsFromDB())
+                }
             }
         })
     }

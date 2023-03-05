70c3f7efc3e38bc9b95e51d733d844b1379b9eb5
PseudoNIMB <ienndevld@yandex.ru>
2023-03-05 14:13:46 +0300

    Рефакторинг MainRepository.kt

--- app/src/main/java/ru/pseudonimb/filmsearch/data/MainRepository.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/data/MainRepository.kt
@@ -8,10 +8,7 @@ import java.util.concurrent.Executors
 class MainRepository(private val filmDao: FilmDao) {
 
     fun putToDb(films: List<Film>) {
-        //Запросы в БД должны быть в отдельном потоке
-        Executors.newSingleThreadExecutor().execute {
-            filmDao.insertAll(films)
-        }
+        filmDao.insertAll(films)
     }
 
     fun getAllFromDB(): Observable<List<Film>> = filmDao.getCachedFilms()

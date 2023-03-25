e49b4d87fd1ac39ed5a0ae23cd12ac4197feb318
PseudoNIMB <ienndevld@yandex.ru>
2023-03-25 15:34:59 +0300

    продолжение работы с ресивером

--- app/src/main/java/ru/pseudonimb/filmsearch/recievers/ConnectionChecker.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/recievers/ConnectionChecker.kt
@@ -3,9 +3,18 @@ package ru.pseudonimb.filmsearch.recievers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class ConnectionChecker : BroadcastReceiver() {
-    override fun onReceive(p0: Context?, p1: Intent?) {
-        TODO("Not yet implemented")
+    override fun onReceive(context: Context?, intent: Intent?) {
+        //Если интента нет, то выходим из метода
+        if (intent == null) return
+        //Проверяем, какой пришел action
+        when (intent.action) {
+            //Если пришел низкий заряд батарее
+            Intent.ACTION_BATTERY_LOW -> Toast.makeText(context, "Батарея разряжена", Toast.LENGTH_SHORT).show()
+            //Если пришло подключение к зарядке
+            Intent.ACTION_POWER_CONNECTED -> Toast.makeText(context, "Зарядка подключена", Toast.LENGTH_SHORT).show()
+        }
     }
 }

3cf6ca19712091d97641bbbe365a526c5d2bc3d7
PseudoNIMB <ienndevld@yandex.ru>
2023-03-25 15:29:12 +0300

    создание проверятеля соединения

---
+++ app/src/main/java/ru/pseudonimb/filmsearch/recievers/ConnectionChecker.kt
@@ -0,0 +1,11 @@
+package ru.pseudonimb.filmsearch.recievers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ConnectionChecker : BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        TODO("Not yet implemented")
+    }
+}

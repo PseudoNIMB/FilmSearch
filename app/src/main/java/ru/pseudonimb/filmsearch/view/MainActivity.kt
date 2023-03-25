bb2732254e9424652bdc2621002ca6d7fb916fd5
PseudoNIMB <ienndevld@yandex.ru>
2023-03-25 15:32:57 +0300

    регистрация ресивера

--- app/src/main/java/ru/pseudonimb/filmsearch/view/MainActivity.kt
+++ app/src/main/java/ru/pseudonimb/filmsearch/view/MainActivity.kt
@@ -2,17 +2,22 @@
 
 package ru.pseudonimb.filmsearch.view
 
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import ru.pseudonimb.filmsearch.R
 import ru.pseudonimb.filmsearch.data.entity.Film
 import ru.pseudonimb.filmsearch.databinding.ActivityMainBinding
+import ru.pseudonimb.filmsearch.recievers.ConnectionChecker
 import ru.pseudonimb.filmsearch.view.fragments.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var reciever: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +25,12 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        reciever = ConnectionChecker()
+        val filters = IntentFilter().apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_BATTERY_LOW)
+        }
+        registerReceiver(reciever, filters)
 
         initNavigation()
 
@@ -98,5 +109,10 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.fragment_placeholder, fragment, tag)
             .addToBackStack(null)
             .commit()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(reciever)
     }
 }
